78161842944e26e49544578315e5c2da4d31e2da
Colin Smith <colin.cs.smith@gmail.com>
2020-01-31 12:19:09 +0100

    update anterior laxity gif

--- opensim-jam-release/examples/anterior_laxity/run_anterior_laxity.cmd
+++ opensim-jam-release/examples/anterior_laxity/run_anterior_laxity.cmd
@@ -1,3 +1,8 @@
+REM ===========================
+REM Run Anterior Laxity Example
+REM ===========================
+
+
 set BIN=%CD%\..\..\bin
 set OPENSIM=%CD%\..\..\opensim
 
@@ -5,9 +10,9 @@ REM Let Windows know where the plugin and opensim libraries are
 set PATH=%BIN%;%OPENSIM%;%PATH%
 
 REM Perform Forward Simulation - Healthy
-%BIN%\forsim %BIN%\jam_plugin.dll .\inputs\forsim_settings.xml
-move out.log results\forsim_out.log
-move err.log results\forsim_err.log
+REM %BIN%\forsim %BIN%\jam_plugin.dll .\inputs\forsim_settings.xml
+REM move out.log results\forsim_out.log
+REM move err.log results\forsim_err.log
 
 REM Perform Joint Mechanics Analysis - Healthy
 %BIN%\joint-mechanics %BIN%\jam_plugin.dll .\inputs\joint_mechanics_settings.xml 
@@ -15,9 +20,9 @@ move out.log results\joint_mechanics_out.log
 move err.log results\joint_mechanics_err.log
 
 REM Perform Forward Simulation - ACL Deficient 
-%BIN%\forsim %BIN%\jam_plugin.dll .\inputs\forsim_settings_acld.xml
-move out.log results\forsim_acld_out.log
-move err.log results\forsim_acld_err.log
+REM %BIN%\forsim %BIN%\jam_plugin.dll .\inputs\forsim_settings_acld.xml
+REM move out.log results\forsim_acld_out.log
+REM move err.log results\forsim_acld_err.log
 
 REM Perform Joint Mechanics Analysis - ACL Deficient
 %BIN%\joint-mechanics %BIN%\jam_plugin.dll .\inputs\joint_mechanics_settings_acld.xml 
