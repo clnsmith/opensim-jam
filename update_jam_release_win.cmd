8f8ac8066947fcb351403f847fee5c2f67844fc5
Colin Smith <colin.cs.smith@gmail.com>
2020-06-02 13:32:46 +0200

    fixed update_jam_release_win.cmd to copy to new windows location

--- update_jam_release_win.cmd
+++ update_jam_release_win.cmd
@@ -1,9 +1,9 @@
 REM Copy C++ executables and libraries
-copy ..\install\plugin\jam_plugin.dll opensim-jam-release\bin\
-copy ..\install\cmd_tools\comak.exe opensim-jam-release\bin\
-copy ..\install\cmd_tools\comak-inverse-kinematics.exe opensim-jam-release\bin\
-copy ..\install\cmd_tools\forsim.exe opensim-jam-release\bin\
-copy ..\install\cmd_tools\joint-mechanics.exe opensim-jam-release\bin\
+copy ..\install\plugin\jam_plugin.dll opensim-jam-release\bin\win
+copy ..\install\cmd_tools\comak.exe opensim-jam-release\bin\win
+copy ..\install\cmd_tools\comak-inverse-kinematics.exe opensim-jam-release\bin\win
+copy ..\install\cmd_tools\forsim.exe opensim-jam-release\bin\win
+copy ..\install\cmd_tools\joint-mechanics.exe opensim-jam-release\bin\win
 
 REM Copy Models
 xcopy /I /y  .\models\lenhart2015  opensim-jam-release\models\lenhart2015 
@@ -16,5 +16,4 @@ xcopy /I /y  .\models\smith2019\Geometry  opensim-jam-release\models\smith2019\G
 xcopy /I /y  .\models\smith2019\motion_data  opensim-jam-release\models\smith2019\motion_data
 xcopy /I /y  .\models\smith2019\graphics  opensim-jam-release\models\smith2019\graphics
 
-REM Copy Documentation
 pause
